570d71584a69ec0c59a514c2dcf8e5dfdc7a0e6a
Tommy Lopes <tommy.lopes@proctors.co.uk>
2021-10-29 15:36:36 +0100

    Change project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
     it('should display welcome message', async () => {
         await page.navigateTo();
-        expect(await page.getTitleText()).toEqual('angular-babylonjs app is running!');
+        expect(await page.getTitleText()).toEqual('psceu-solutions-drawings app is running!');
     });
 
     afterEach(async () => {
